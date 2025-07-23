426af3480f22cfd0b9a7defadb9bc5e3471ed657
github.com/PRINCE-droid412 <Princetakyijnr@yahoo.com>
2025-07-23 03:29:31 -0400

    interfaces files & 404 page resolved

---
+++ alx-project-0x03/interface/index.tsx
@@ -0,0 +1,21 @@
+// import { PageRouteProps } from "@/interface";
+import { ReactNode } from "react"
+
+
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+
+export interface LayoutProps {
+  children: ReactNode;
+}
